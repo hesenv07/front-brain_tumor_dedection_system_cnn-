aca5d79ea957f9d099727fec723c77387087f3e1
arif hasanov <arif.hasanov@ailab.az>
2025-03-24 00:18:11 +0400

    add base url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_BASE_URL: 'http://127.0.0.1:8000',
+    NEXT_PUBLIC_BASE_URL: 'https://brain-tumor-dedection-system-cnn.onrender.com',
   }
 };
 
